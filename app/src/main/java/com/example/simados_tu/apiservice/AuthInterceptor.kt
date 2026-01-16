38dfd6ad52103e508982160ede2f10fd6e1be5c6
nabilasaf <nabilasafitri861@gmail.com>
2026-01-17 04:21:11 +0700

    Menambahkan `AuthInterceptor` untuk menyertakan token otentikasi.

---
+++ app/src/main/java/com/example/simados_tu/apiservice/AuthInterceptor.kt
@@ -0,0 +1,25 @@
+package com.example.simados_tu.apiservice
+
+
+import com.example.simados_tu.repositori.TokenManager
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(
+    private val tokenManager: TokenManager
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = tokenManager.getTokenSync()
+
+        val request = if (!token.isNullOrEmpty()) {
+            chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+        } else {
+            chain.request()
+        }
+
+        return chain.proceed(request)
+    }
+}
